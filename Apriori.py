0d3cce1d37bb0b49ad442d6c8f316d2f7143b334
lpuatu <54289824+lpuatu@users.noreply.github.com>
2022-02-21 11:09:38 -0600

    Update Apriori.py
    loads data into itemSet[][]

--- Apriori.py
+++ Apriori.py
@@ -3,14 +3,15 @@ from scipy.io import arff
 data, meta = arff.loadarff('supermarket.arff')
 itemSet = [[]]
 
+id = 0
+for f in meta:
+    itemSet.insert(id, [f, 0])
 
-for i in range(columns):
-    for j in range(row):
-        if(j == 't'):
-            count = count + 1
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        if data[i][j] == 't':
+            count = itemSet[j][1]
+            itemSet[j][1] = count + 1
 
-print count
-
-for k in data:
-
-    sum(x.count('t') for x in i)
+for x in itemSet:
+    print x
