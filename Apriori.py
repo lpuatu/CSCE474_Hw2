20e1464f1cf1f3f7a9b1d09394c41ad9d92001fa
lpuatu <54289824+lpuatu@users.noreply.github.com>
2022-02-22 16:28:29 -0600

    Update Apriori.py
    Apriori Algorithm fully implemented

--- Apriori.py
+++ Apriori.py
@@ -1,75 +1,85 @@
 from scipy.io import arff
 from array import *
-#The Apriori Algorithm for a given file
 
-def genereateRules(minsupport, minconfidence):
-    return ""
+#Reads the .arff file
+#fileName = input('Enter file name: ')
+data, meta = arff.loadarff('supermarket.arff')
+#Initializes the base itemSet and frequent item set
+columns = []
+itemSet = []
+firstSet = []
+freqSet = []
+previousfreqSet = []
+totalCount = 0
+#Creates the minimum support and confidence values
+minSup = input('Enter minimum support: ')
+minConf = 500
+dataTrue = 't'
+#Inserts the base items to the itemsets at value 0
+for a in meta:
+    itemSet.append([a, 0])
+    columns.append(a)
+#Iterates through the dataset and counts each occurence of an item, updates the information
+for b in range(len(data)):
+    for c in range(len(data[b])):
+        if data[b][c] == dataTrue:
+            count = itemSet[c][1]
+            itemSet[c][1] = count + 1
+    totalCount = totalCount + 1
+#If the items in the set are above the min_sup count then the item is appended to the firstSet
 
-def generateSubItemsets(minsupport, minconfidence, data, columns, data_list):
-    #data list [0] is position in columns data list [2] is the value of that it has
-    count = 1
-    
+for d in itemSet:
+    if d[1] > minSup:
+        firstSet.append(d[0])
 
+#Adds The first set into the freqSet as pairs
+for e in firstSet:
+    for f in firstSet:
+        #Checks if the pair being looked to add already exists or is non entry ie x,x or y,y
+        if [f,e] not in freqSet and e != f:
+            pairSet = [e,f]
+            h = columns.index(e)
+            i = columns.index(f)
+            occurence = 0
+            for g in data:
+                if g[h] == dataTrue and g[i] == dataTrue:
+                    occurence = occurence + 1
+                if occurence > minSup and pairSet not in freqSet:
+                    freqSet.append(pairSet)
+                    previousfreqSet.append(pairSet)
+                    break
 
-    return ""
+boolean = True
+while boolean:
+    boolean = False
+    addedSet = []
+    for k in previousfreqSet:
+        for l in firstSet:
+            newSet = list(k)
+            if l not in newSet:
+                newSet.append(l)
+                newCount = 0
+                for n in data:
+                    for m in newSet:
+                        if n[columns.index(m)] == dataTrue:
+                            verify = True
+                        else:
+                            verify = False
+                            break
+                    if verify:
+                        newCount = newCount + 1
+                if newCount >= minSup:
+                    freqSet.append(newSet)
+                    addedSet.append(newSet)
+                    boolean = True
+    previousfreqSet = []
+    previousfreqSet = addedSet
 
-# def dataRowsToColumns(data,meta):
-#     for 
+sortedFreq = [frozenset(o) for o in freqSet]
+cleaned_set = set(sortedFreq)
 
-def main():
-    minsupport = input("What is the minimum support for this data?\n")
-    minconfidence = input("What is the minimum confidence for this data\n")
-    filename = input("What is the filename for this data. Note that it must be in arff format, and the same directory as Apriori.py\n")
-    # print(minsupport)
-    # print(minconfidence)
-    #loadarff supermarket.arff
-    data, meta = arff.loadarff(filename)
-    data_list = []
-    data_dict = {}
-    id = 0
-    # print(data)
-    columns =[]
-    
-    for f in meta:
-        columns.append(f)
-
-    for i in range(len(columns)):
-        tempCol = []
-        
-        for j in range(len(data)):
-            tempCol.append(data[j][i])
-        tempSet = set(tempCol)
-        tempTup = []
-        for k in tempSet:
-            if tempCol.count(k)/len(data) > float(minsupport):
-                tempTup.append([i,columns[i],k,tempCol.count(k), tempCol.count(k)/len(data)])
-        data_dict[columns[i]] = tempCol
-        data_list.extend(tempTup)
-    #For data_list it is [number,name, value, number of times in elements in data, support]
-    # print(data_list)
-    print("Minimum support: " + minsupport)
-    print("Minimum confidence: " + minconfidence + "\n")
-    print("Size of large itemset L(1): "+str(len(data_list)))
-    print(data_list)
-    
-    
-    secondlist = []
-    for x in range(len(data_list)):
-        for y in range(x,len(data_list)):
-            supconcount = 0
-            if (x != y):
-                for i in range(len(data)):
-                    #The element that has to change is the data_list[x][0] and data_list[y][0]
-                    
-                    if ((data[i][data_list[x][0]] == data_list[x][2]) and (data[i][data_list[y][0]] == data_list[y][2])):
-                        supconcount += 1
-                support = supconcount/len(data)
-                if(support > float(minsupport)):
-                    secondlist.append([data_list[x],data_list[y],support])
-    print("Size of large itemset L(2):" +str(len(secondlist)))
-    print(secondlist)
-        
-    
-
-if __name__ == "__main__":
-    main()
+testCount = 0
+for j in cleaned_set:
+    print list(j)
+    testCount =  testCount + 1
+print testCount
