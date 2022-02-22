b92dde2e84de0684e2f4cb0a2cfd0407ea9559cd
lpuatu <54289824+lpuatu@users.noreply.github.com>
2022-02-22 15:40:07 -0600

    Update Apriori.py
    Almost completes l3

--- Apriori.py
+++ Apriori.py
@@ -8,10 +8,12 @@ columns = []
 itemSet = []
 firstSet = []
 freqSet = []
+previousfreqSet = []
 totalCount = 0
 #Creates the minimum support and confidence values
-minSup = 1000
+minSup = 2000
 minConf = 500
+dataTrue = 't'
 #Inserts the base items to the itemsets at value 0
 for a in meta:
     itemSet.append([a, 0])
@@ -19,38 +21,59 @@ for a in meta:
 #Iterates through the dataset and counts each occurence of an item, updates the information
 for b in range(len(data)):
     for c in range(len(data[b])):
-        if data[b][c] == 't':
+        if data[b][c] == dataTrue:
             count = itemSet[c][1]
             itemSet[c][1] = count + 1
     totalCount = totalCount + 1
 #If the items in the set are above the min_sup count then the item is appended to the firstSet
-try:
-    d = 0
-    while d <= len(itemSet):
-        if itemSet[d][1] > minSup:
-            firstSet.append(itemSet[d])
-        d = d + 1
-except:
-    print ""
+
+for d in itemSet:
+    if d[1] > minSup:
+        firstSet.append(d[0])
 
 #Adds The first set into the freqSet as pairs
 for e in firstSet:
     for f in firstSet:
         #Checks if the pair being looked to add already exists or is non entry ie x,x or y,y
-        if [f[0],e[0]] not in freqSet and e[0] != f[0]:
-            pairSet = [e[0],f[0]]
-            h = columns.index(e[0])
-            i = columns.index(f[0])
+        if [f,e] not in freqSet and e != f:
+            pairSet = [e,f]
+            h = columns.index(e)
+            i = columns.index(f)
             occurence = 0
             for g in data:
-                if g[h] == 't' and g[i] == 't':
+                if g[h] == dataTrue and g[i] == dataTrue:
                     occurence = occurence + 1
                 if occurence > minSup and pairSet not in freqSet:
                     freqSet.append(pairSet)
+                    previousfreqSet.append(pairSet)
+                    break
+
+boolean = True
+while boolean:
+    boolean = False
+    for k in previousfreqSet:
+        newSet = list(k)
+        for l in firstSet:
+            if l not in newSet:
+                newSet.append(l)
+                newCount = 0
+                for n in data:
+                    for m in newSet:
+                        if n[columns.index(m)] == dataTrue:
+                            verify = True
+                        else:
+                            verify = False
+                            break
+                    if verify:
+                        newCount = newCount + 1
+                if newCount >= minSup:
+                    freqSet.append(newSet)
+                    #boolean = True
+
+
 
 testCount = 0
 for j in freqSet:
     print j
     testCount =  testCount + 1
 print testCount
-print totalCount
