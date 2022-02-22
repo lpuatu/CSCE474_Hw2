cbf7a66e337bbb13566d4bb9315d871bc877ed47
alexrechsteinerUNL <alex.rechsteiner@huskers.unl.edu>
2022-02-21 18:23:39 -0600

    refactored the code and added some testing data have not started the math part yet

--- Apriori.py
+++ Apriori.py
@@ -1,19 +1,35 @@
 from scipy.io import arff
 from array import *
+#The Apriori Algorithm for a given file
 
-data, meta = arff.loadarff('supermarket.arff')
-itemSet = [[]]
+def genereateRules(minsupport, minconfidence):
+    return ""
 
-id = 0
-for f in meta:
-    itemSet.insert(id, [f, 0])
-    id = id + 1
+def generateItemsets(minsupport, minconfidence):
+    return ""
 
-for i in range(len(data)):
-    for j in range(len(data[i])):
-        if data[i][j] == 't':
-            count = itemSet[j][1]
-            itemSet[j][1] = count + 1
+def main():
+    minsupport = input("What is the minimum support for this data?\n")
+    minconfidence = input("What is the minimum confidence for this data\n")
+    filename = input("What is the filename for this data. Note that it must be in arff format, and the same directory as Apriori.py\n")
+    print(minsupport)
+    print(minconfidence)
+    #loadarff supermarket.arff
+    data, meta = arff.loadarff(filename)
+    itemSet = [[]]
+    id = 0
+    for f in meta:
+        itemSet.insert(id, [f, 0])
+        id = id + 1
 
-for x in itemSet:
-    print x
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j] == 't':
+                count = itemSet[j][1]
+                itemSet[j][1] = count + 1
+
+    for x in itemSet:
+        print(x)
+
+if __name__ == "__main__":
+    main()
